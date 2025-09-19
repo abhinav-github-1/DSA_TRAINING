92d28a960d245a9f9d2f6253a9a73a9ca1cf2b47
Kuwar Abhinav Singh <singhkuwarabhinav@gmail.com>
2025-09-19 09:56:06 +0530

    inbuilt function for rowwise sum

--- rowSumMaxMin.java
+++ rowSumMaxMin.java
@@ -12,18 +12,26 @@ public class rowSumMaxMin {
             arr[i][j]=sc.nextInt();
         }
        } 
-       for(int i=0;i<n;i++)
-       {
-        int sum=0;
-        int max=Integer.MIN_VALUE;
-        int min=Integer.MAX_VALUE;
-        for(int j=0;j<m;j++)
-        {
-            sum+=arr[i][j];
-            max=Math.max(max,arr[i][j]);
-            min=Math.min(min,arr[i][j]);
-        }
-        System.out.println(sum+" "+max+" "+ min);
-       }
+    //    for(int i=0;i<n;i++)
+    //    {
+    //     int sum=0;
+    //     int max=Integer.MIN_VALUE;
+    //     int min=Integer.MAX_VALUE;
+    //     for(int j=0;j<m;j++)
+    //     {
+    //         sum+=arr[i][j];
+    //         max=Math.max(max,arr[i][j]);
+    //         min=Math.min(min,arr[i][j]);
+    //     }
+    //     System.out.println(sum+" "+max+" "+ min);
+    //    }
+    for(int i=0;i<n;i++)
+    {
+        int sum=Arrays.stream(arr[i]).sum();
+        int max=Arrays.stream(arr[i]).max().getAsInt();
+        int min=Arrays.stream(arr[i]).min().getAsInt();
+        System.out.println(sum+" "+max+" "+min);
+    }
+    
     }
 }

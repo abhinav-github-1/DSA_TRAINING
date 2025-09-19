a41e07420fb019fbcdf1a842ffa1905b1f362fe4
Kuwar Abhinav Singh <singhkuwarabhinav@gmail.com>
2025-09-19 09:40:55 +0530

    kadane's algo

--- maxSubarraySum.java
+++ maxSubarraySum.java
@@ -8,16 +8,26 @@ public class maxSubarraySum {
         {
            arr[i]=sc.nextInt();
         }
-        int res=0;
-        for(int i=0;i<n-1;i++)
-        {
-            int sum=arr[i];
-            for(int j=i+1;j<n;j++)
-            {
-                sum+=arr[j];
+        // int res=0;
+        // for(int i=0;i<n-1;i++)
+        // {
+        //     int sum=arr[i];
+        //     for(int j=i+1;j<n;j++)
+        //     {
+        //         sum+=arr[j];
                 
-            }
-            res=Math.max(sum,res);
+        //     }
+        //     res=Math.max(sum,res);
+        // }
+        // System.out.println(res);
+
+        //Kadane's Algorithm
+        int res=arr[0];
+        int max=arr[0];
+        for(int i=1;i<n;i++)
+        {
+            max=Math.max(max+arr[i],arr[i]);
+            res=Math.max(max,res);
         }
         System.out.println(res);
     }

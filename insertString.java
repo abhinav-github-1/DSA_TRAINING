bd1555d7f95f75e376f9891d9997f7e36b25a6a1
Kuwar Abhinav Singh <singhkuwarabhinav@gmail.com>
2025-09-23 15:19:46 +0530

    inserting string at intervals

---
+++ insertString.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class insertString {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        String s1=sc.nextLine();
+        String s2=sc.nextLine();
+        int n=sc.nextInt();
+        String res="";
+        for(int i=0;i<s1.length();i++)
+        {
+            if(i%n==0&&i!=0)
+            res+=(s2+s1.charAt(i));
+            else
+            res+=s1.charAt(i);
+        }
+        System.out.println(res);
+         
+
+    }
+    
+}
